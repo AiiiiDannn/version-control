b5405186d0f870e6ca8e838558defb962d8d530b
Aiden Hai <xhai2@uci.edu>
2025-01-25 08:03:00 +0000

    Add main.py and version.md

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+# Get current date and time
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# Write to version.md
+with open('../version.md', 'w') as file:
+    file.write(f'Last updated: {current_time}\n')
